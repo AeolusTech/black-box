72d68de7f84883e929087f8991e03736cdee4824
Kamil Kuczaj <kamil.kuczaj@gmail.com>
2022-01-12 21:48:57 +0100

    GPS data is collected
    Time for a test drive

--- isoml/isoml/gps/__init__.py
+++ isoml/isoml/gps/__init__.py
@@ -35,24 +35,28 @@ import time
 class GPS(threading.Thread):
 	ser = serial.Serial("/dev/ttyS0",115200)
 	
-	RMC_LAT_INDEX = 61
-	RMC_LONG_INDEX = 62
+	GGA_LAT_INDEX = 2
+	GGA_NORTH_SOUTH_HEMISPEHERE = 3
+	GGA_LONG_INDEX = 4
+	GGA_EAST_WEST_HEMISPEHERE = 5
 
-	W_buff = ["AT+CGNSPWR=0\r\n", # power off GNSS
-		  "AT+CGNSPWR=1\r\n", # power on GNSS
-		  "AT+CGNSSEQ=\"RMC\"\r\n", # define the last NMEA sentence that parsed
-		  # NMEA - National Marine Electronics Association
-		  # RMC - Recommended Minimum Specific GNSS Data
-          "AT+CGNSINF\r\n", # read GNSS navigation information
-		  "AT+CGNSURC=2\r\n", # set Unsolicited Result Code reporting every 2 GNSS fix 
-		  "AT+CGNSTST=1\r\n" # send NMEA data to AT UART
-		]
+	W_buff = [
+		"AT+CGNSPWR=0\r\n", # power off GNSS
+		"AT+CGNSPWR=1\r\n", # power on GNSS
+		"AT+CGNSSEQ=\"GGA\"\r\n", # define the last NMEA sentence that parsed
+		# NMEA - National Marine Electronics Association
+		# GGA - Global Positioning System Fix Data
+		# GSA - GNSS DOP and Active Satellites
+		# GSV - GNSS Satellites in View
+		# RMC - Recommended Minimum Specific GNSS Data
+		"AT+CGNSINF\r\n", # read GNSS navigation information
+		"AT+CGNSURC=0\r\n", # set Unsolicited Result Code reporting every 0 GNSS fix 
+		"AT+CGNSTST=1\r\n" # send NMEA data to AT UART
+	]
+
 	
-	data_len = len(W_buff) - 1
-	data = ""
-	num = 0
-	lat = 0
-	long = 0
+	latitude = 0
+	longitude = 0
 
 
 	def __init__(self):
@@ -62,44 +66,81 @@ class GPS(threading.Thread):
 		
 	def run(self):
 		try:
+			data_len = len(self.W_buff) - 1
+			data = ""
+			num = 0
 			while True:
 				if self.ser.inWaiting() == 0:
-					# print ("Initializing")
 					time.sleep(0.5)
 				else:
 					while self.ser.inWaiting() > 0:
-						self.data += self.ser.read(self.ser.inWaiting()).decode()
-					if self.data != "":
-						print(self.data)
-						if  self.num < self.data_len:
-							# print(self.num)
+						data += self.ser.read(self.ser.inWaiting()).decode()
+					if data != "":
+						print(data)
+						if  num < data_len:
 							time.sleep(0.5)
-							self.ser.write(self.W_buff[self.num+1].encode())
-						if self.num == self.data_len:
+							self.ser.write(self.W_buff[num+1].encode())
+						if num == data_len:
 							time.sleep(0.5)
-							self.ser.write(self.W_buff[self.data_len].encode())
-						if self.num > self.data_len:
-							if 'GNRMC' in self.data:
-								self.lat, self.long = self.parse_and_return_lat_long(self.data)
-						self.data = ""
-						self.num = self.num +1
+							self.ser.write(self.W_buff[data_len].encode())
+						if num > data_len:
+							if "GGA" in data:
+								self.latitude, self.longitude = self.parse_and_return_lat_long(data)
+						data = ""
+						num =num +1
 		finally:
 			if self.ser != None:
 				self.ser.close()
 
+	def get_labels(self):
+		return ['lat', 'long']
+
+	def get_data(self):
+		return [self.latitude, self.longitude]
 
 	def parse_and_return_lat_long(self, data):
 		'''
 		Response example:
 		1,0,19800106000115.093,,,,0.00,0.0,0,,,,,,0,0,,,,,
 		'''
+
 		split_data = data.split(',')
-		lat = split_data[self.RMC_LAT_INDEX]
-		long = split_data[self.RMC_LONG_INDEX]
-		return lat, long
+		lat_nmea_string = split_data[self.GGA_LAT_INDEX]
+		is_south = split_data[self.GGA_NORTH_SOUTH_HEMISPEHERE] == 'S'
+		
+		
+		long_nmea_string = split_data[self.GGA_LONG_INDEX]
+		is_west = split_data[self.GGA_EAST_WEST_HEMISPEHERE] == 'W'
 
-	def get_labels(self):
-		return ['lat', 'long']
 
-	def get_data(self):
-		return [self.lat, self.long]
+		latitude = None
+		longitude = None
+
+		if long_nmea_string and lat_nmea_string:
+			latitude = self.NmeaToDecimal_lat(lat_nmea_string, is_south)
+			longitude = self.NmeaToDecimal_long(long_nmea_string, is_west)
+		
+		return latitude, longitude
+
+
+	def NmeaToDecimal_lat(self, lat_string, is_south):
+		'''
+		https://stackoverflow.com/questions/36254363/how-to-convert-latitude-and-longitude-of-nmea-format-data-to-decimal
+		'''
+		lat_degrees = int(lat_string[0:2])
+		lat_minutes = float(lat_string[2:])
+
+		latitude = lat_degrees + lat_minutes/60.0
+
+		if is_south:
+			return -1.0 * latitude
+		return latitude
+
+
+	def NmeaToDecimal_long(self, long_string, is_west):
+		long_degrees = int(long_string[0:3])
+		long_minutes = float(long_string[3:])
+		longitude = long_degrees + long_minutes/60.0
+		if is_west:
+			return -1.0 * longitude
+		return longitude
