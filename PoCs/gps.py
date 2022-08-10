6c7759a2dc4b3d751c72cae1a568428279101739
Kamil Kuczaj <kamil.kuczaj@aifi.com>
2022-08-11 00:34:21 +0200

    finally something nice created regarding wrapper to serial communication

--- PoCs/gps.py
+++ PoCs/gps.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 # Filename: text.py
-import serial
-import time
 
+import asyncio
+import serial_asyncio
 
 # GPS response data
 #
@@ -31,14 +31,9 @@ import time
 #  RMC – Recommended Minimum Specific GNSS Data
 
 
-ser = serial.Serial("/dev/ttyS0", baudrate=115200)
 
-W_buff = [
-"AT+SAPBR=3,1,\"CONTYPE\",\"GPRS\"\r\n",  # Set bearer parameter
-"AT+SAPBR=3,1,\"APN\",\"internet\"\r\n",   # Set bearer context
-"AT+SAPBR=1,1",                           # Active bearer context
-"AT+SAPBR=2,1"							  # Read bearer parameter
-]
+
+
 
 GGA_LAT_INDEX = 2
 GGA_NORTH_SOUTH_HEMISPEHERE = 3
@@ -46,79 +41,160 @@ GGA_LONG_INDEX = 4
 GGA_EAST_WEST_HEMISPEHERE = 5
 
 
-data_len = len(W_buff) - 1
-
-ser.write(W_buff[0].encode())
-ser.flushInput()
-data = ""
-num = 0
 
 def parse_and_return_lat_long(data):
-	'''
-	Response example:
-	1,0,19800106000115.093,,,,0.00,0.0,0,,,,,,0,0,,,,,
-	'''
+    '''
+    Response example:
+    1,0,19800106000115.093,,,,0.00,0.0,0,,,,,,0,0,,,,,
+    '''
 
-	split_data = data.split(',')
-	lat_nmea_string = split_data[GGA_LAT_INDEX]
-	is_south = split_data[GGA_NORTH_SOUTH_HEMISPEHERE] == 'S'
-
-
-	long_nmea_string = split_data[GGA_LONG_INDEX]
-	is_west = split_data[GGA_EAST_WEST_HEMISPEHERE] == 'W'
+    split_data = data.split(',')
+    lat_nmea_string = split_data[GGA_LAT_INDEX]
+    is_south = split_data[GGA_NORTH_SOUTH_HEMISPEHERE] == 'S'
 
 
-	latitude = None
-	longitude = None
+    long_nmea_string = split_data[GGA_LONG_INDEX]
+    is_west = split_data[GGA_EAST_WEST_HEMISPEHERE] == 'W'
 
-	if long_nmea_string and lat_nmea_string:
-		latitude = NmeaToDecimal_lat(lat_nmea_string, is_south)
-		longitude = NmeaToDecimal_long(long_nmea_string, is_west)
 
-	return latitude, longitude
+    latitude = None
+    longitude = None
+
+    if long_nmea_string and lat_nmea_string:
+        latitude = NmeaToDecimal_lat(lat_nmea_string, is_south)
+        longitude = NmeaToDecimal_long(long_nmea_string, is_west)
+
+    return latitude, longitude
 
 
 def NmeaToDecimal_lat(lat_string, is_south):
-	'''
-	https://stackoverflow.com/questions/36254363/how-to-convert-latitude-and-longitude-of-nmea-format-data-to-decimal
-	'''
-	lat_degrees = int(lat_string[0:2])
-	lat_minutes = float(lat_string[2:])
+    '''
+    https://stackoverflow.com/questions/36254363/how-to-convert-latitude-and-longitude-of-nmea-format-data-to-decimal
+    '''
+    lat_degrees = int(lat_string[0:2])
+    lat_minutes = float(lat_string[2:])
 
-	latitude = lat_degrees + lat_minutes/60.0
+    latitude = lat_degrees + lat_minutes/60.0
 
-	if is_south:
-		return -1.0 * latitude
-	return latitude
+    if is_south:
+        return -1.0 * latitude
+    return latitude
 
 
 def NmeaToDecimal_long(long_string, is_west):
-	long_degrees = int(long_string[0:3])
-	long_minutes = float(long_string[3:])
-	longitude = long_degrees + long_minutes/60.0
-	if is_west:
-		return -1.0 * longitude
-	return longitude
-
-def read_serial() -> str:
-	while (True):
-		if (ser.inWaiting() > 0):
-			data_str = ser.read(ser.inWaiting()).decode('ascii')
-			print(data_str, end='')
-			return data_str
-
-		time.sleep(0.1)  # sleep 100ms
-
-def write_serial(data: str):
-	ser.write(data.encode())
+    long_degrees = int(long_string[0:3])
+    long_minutes = float(long_string[3:])
+    longitude = long_degrees + long_minutes/60.0
+    if is_west:
+        return -1.0 * longitude
+    return longitude
 
 
-try:
-	for i in range(len(W_buff)):
-		write_serial(W_buff[i])
-		_ = read_serial()
 
-except KeyboardInterrupt:
-	if ser != None:
-		ser.close()
 
+
+port = "/dev/ttyS0"
+baud = 115200
+
+
+# class Writer(asyncio.Protocol):
+#     def connection_made(self, transport):
+#         """Store the serial transport and schedule the task to send data.
+#         """
+#         self.transport = transport
+#         print('Writer connection created')
+#         asyncio.ensure_future(self.send())
+#         print('Writer.send() scheduled')
+
+#     def connection_lost(self, exc):
+#         print('Writer closed')
+
+#     async def send(self):
+#         """Send four newline-terminated messages, one byte at a time.
+#         """
+#         buffer = [
+# "AT+SAPBR=3,1,\"CONTYPE\",\"GPRS\"\r\n",  # Set bearer parameter
+# "AT+SAPBR=3,1,\"APN\",\"internet\"\r\n",   # Set bearer context
+# "AT+SAPBR=1,1",                           # Active bearer context
+# "AT+SAPBR=2,1"							  # Read bearer parameter
+# ]
+#         for message in buffer:
+#             await asyncio.sleep(0.5)
+#             bmessage = message.encode()
+#             self.transport.serial.write(bmessage)
+#             print(f'Writer sent: {bmessage}')
+#         self.transport.close()
+
+
+# class Reader(asyncio.Protocol):
+#     def connection_made(self, transport):
+#         """Store the serial transport and prepare to receive data.
+#         """
+#         self.transport = transport
+#         self.buf = bytes()
+#         self.msgs_recvd = 0
+#         print('Reader connection created')
+
+#     def data_received(self, data):
+#         """Store characters until a newline is received.
+#         """
+#         self.buf += data
+#         if b'\n' in self.buf:
+#             lines = self.buf.split(b'\n')
+#             self.buf = lines[-1]  # whatever was left over
+#             for line in lines[:-1]:
+#                 print(f'Reader received: {line.decode()}')
+#                 self.msgs_recvd += 1
+#         if self.msgs_recvd == 4:
+#             self.transport.close()
+
+#     def connection_lost(self, exc):
+#         print('Reader closed')
+
+
+# loop = asyncio.get_event_loop()
+# reader = serial_asyncio.create_serial_connection(loop, Reader, port, baudrate=baud)
+# writer = serial_asyncio.create_serial_connection(loop, Writer, port, baudrate=baud)
+# asyncio.ensure_future(reader)
+# print('Reader scheduled')
+# asyncio.ensure_future(writer)
+# print('Writer scheduled')
+# loop.call_later(10, loop.stop)
+# loop.run_forever()
+# print('Done')
+
+async def main(loop):
+    reader, writer = await serial_asyncio.open_serial_connection(url=port, baudrate=115200)
+    print('Reader and writer created')
+    messages = [
+b"AT+SAPBR=3,1,\"CONTYPE\",\"GPRS\"\r\n",  # Set bearer parameter
+b"AT+SAPBR=3,1,\"APN\",\"internet\"\r\n",   # Set bearer context
+b"AT+SAPBR=1,1",                           # Active bearer context
+b"AT+SAPBR=2,1"							  # Read bearer parameter
+]
+    sent = send(writer, messages)
+    received = recv(reader)
+    await asyncio.wait([sent, received])
+
+
+async def send(w, msgs):
+    for msg in msgs:
+        w.write(msg)
+        print(f'sent: {msg.decode().rstrip()}')
+        await asyncio.sleep(0.5)
+    w.write(b'DONE\n')
+    print('Done sending')
+
+
+async def recv(r):
+    while True:
+        msg = await r.readuntil(b'\n')
+        if msg.rstrip() == b'DONE':
+            print('Done receiving')
+            break
+        print(f'received: {msg.rstrip().decode()}')
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main(loop))
+loop.close()
