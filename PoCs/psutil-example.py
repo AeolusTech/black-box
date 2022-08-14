a700366bffbe290c3e41ccfbeeff05e92b49556e
Kamil Kuczaj <kamil.kuczaj@aifi.com>
2022-08-14 12:30:30 +0200

    added status message in SMS

---
+++ PoCs/psutil-example.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import psutil
+
+# gives a single float value
+print(psutil.cpu_percent())
+
+# gives an object with many fields
+print(psutil.virtual_memory())
+
+# you can convert that object to a dictionary
+print(dict(psutil.virtual_memory()._asdict()))
+
+# you can have the percentage of used RAM
+print(f'memory usage: psutil.virtual_memory().percent')
+
+
+# you can calculate percentage of available memory
+print(f'free: {psutil.virtual_memory().available * 100 / psutil.virtual_memory().total}')
+
